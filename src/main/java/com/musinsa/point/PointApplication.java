b69c020379df990b4526538644b58104f5d46ed7
wond <wond@gmail.com>
2025-11-13 14:38:06 +0900

    - env 파일 추가 : db 접속정보 - h2 콘솔 접근 허용

--- src/main/java/com/musinsa/point/PointApplication.java
+++ src/main/java/com/musinsa/point/PointApplication.java
@@ -1,5 +1,6 @@
 package com.musinsa.point;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PointApplication {
 
     public static void main(String[] args) {
+        // .env 파일 로드
+        Dotenv dotenv = Dotenv.configure().load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+        
         SpringApplication.run(PointApplication.class, args);
     }
 
--- src/main/java/com/musinsa/point/config/SecurityConfig.java
+++ src/main/java/com/musinsa/point/config/SecurityConfig.java
@@ -1,10 +1,14 @@
 package com.musinsa.point.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -13,12 +17,22 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .authorizeHttpRequests(auth -> auth
-                .anyRequest().permitAll()
+            .csrf(AbstractHttpConfigurer::disable) // CSRF 비활성화
+            .headers(headers -> headers
+                .frameOptions(frameOptions -> frameOptions.sameOrigin()) // H2 콘솔을 위한 X-Frame-Options 설정
             )
-            .csrf(csrf -> csrf.disable());
-        
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers(
+                    new AntPathRequestMatcher("/"),
+                    new AntPathRequestMatcher("/h2-console/**"),
+                    new AntPathRequestMatcher("/swagger-ui.html"),
+                    new AntPathRequestMatcher("/swagger-ui/**"),
+                    new AntPathRequestMatcher("/api-docs/**")
+                ).permitAll()
+                .anyRequest().authenticated()
+            );
+
         return http.build();
     }
 }
-
