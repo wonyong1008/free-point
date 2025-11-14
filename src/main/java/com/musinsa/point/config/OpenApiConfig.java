de6eba295b3038810f766c6db13be1a33dd72b00
wond <wond@gmail.com>
2025-11-14 13:48:08 +0900

    - Swagger UI에서 토큰 입력 후 API 호출 가능하도록 수정

--- src/main/java/com/musinsa/point/config/OpenApiConfig.java
+++ src/main/java/com/musinsa/point/config/OpenApiConfig.java
@@ -3,6 +3,9 @@ package com.musinsa.point.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,6 +15,12 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
+                .components(new Components().addSecuritySchemes("BearerAuth",
+                        new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList("BearerAuth"))
                 .info(new Info()
                         .title("무신사 포인트 시스템 API")
                         .description("무료 포인트 시스템 API 문서")
@@ -21,4 +30,3 @@ public class OpenApiConfig {
                                 .email("payment@musinsa.com")));
     }
 }
-
